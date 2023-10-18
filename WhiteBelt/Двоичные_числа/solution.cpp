15da416888d354a2a0075b5560474c4072ef00b9
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-18 10:22:08 +0300

    Update solution.cpp

--- WhiteBelt/Двоичные_числа/solution.cpp
+++ WhiteBelt/Двоичные_числа/solution.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+void binary(int bin_num){
+	string num = to_string(bin_num);
+	int p = num.length()-1;
+	int res = 0;
+	for(int i = 0; i < num.size(); i++){
+		res += (num[i]-'0') * pow(2, p);
+		p--;
+	}
+	cout << res;
+}
+
+int main(int argc, char *argv[])
+{
+	binary(10110110);
+}
