96f766f18b66e22e3cbd697856768593b29ddc39
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-18 10:21:29 +0300

    Update solution.cpp

--- WhiteBelt/Второе_вхождение/solution.cpp
+++ WhiteBelt/Второе_вхождение/solution.cpp
@@ -1,1 +1,23 @@
+#include <iostream> 
+#include <vector>
 
+using namespace std;
+
+int find_f(string text, char symb){
+	int target = text.find(symb);
+	if(target != -1){
+		return text.find(symb, target+1);
+	}else{
+		return -2;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	string letter = "informative film";
+	string letter2 = "film";
+	string letter3 = "test";
+	cout << find_f(letter, 'f') << endl;
+	cout << find_f(letter2, 'f') << endl;
+	cout << find_f(letter3, 'f') << endl;
+}
