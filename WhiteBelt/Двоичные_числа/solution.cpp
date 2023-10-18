916b3a93d84dd1170014851eb31d718743821657
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-18 10:47:15 +0300

    Update solution.cpp

--- WhiteBelt/Двоичные_числа/solution.cpp
+++ WhiteBelt/Двоичные_числа/solution.cpp
@@ -7,8 +7,8 @@ void binary(int bin_num)
 	string str;
 	while (bin_num > 0)
 	{
-		int ost = bin_num % 2;
-		str += to_string(ost);
+		int remainder = bin_num % 2;
+		str += to_string(remainder);
 		bin_num = bin_num / 2;
 	}
 	reverse(str.begin(), str.end());
