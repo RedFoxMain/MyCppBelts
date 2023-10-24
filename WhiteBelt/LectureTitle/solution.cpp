eda1c038ae35d568976b448c7ef2e414cbeef6f8
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-24 22:43:26 +0300

    Update solution.cpp

--- WhiteBelt/LectureTitle/solution.cpp
+++ WhiteBelt/LectureTitle/solution.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
 
+using namespace std;
+
+struct Specialization
+{
+	string specialization;
+	Specialization(string specialization){
+		specialization = "";
+	}
+};
+
+struct Course
+{
+	string course;
+	Course(string course){
+		course = "";
+	}
+};
+
+struct Week
+{
+	string week;
+	Week(string week){
+		week = "";
+	}
+};
+
+struct LectureTitle
+{
+	string specialization, course, week;
+	
+	LectureTitle(Specialization spec, Course course, Week week){
+		this->specialization = spec.specialization;
+		this->course = course.course;
+		this->week = week.week;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	LectureTitle lec(
+	Specialization("c++"),
+	Course("white belt"),
+	Week("4th"));
+}
