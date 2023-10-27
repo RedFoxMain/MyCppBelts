d5675a02f4edbf23c072aec85c4aadca0df2997b
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-27 21:11:07 +0300

    Update solution.cpp

--- YellowBelt/Ссылка_на_элемент/solution.cpp
+++ YellowBelt/Ссылка_на_элемент/solution.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <map>
+using namespace std;
 
+template<typename N, typename Value>
+Value& GetRefStrict(map<N, Value> &dict, N key){
+	if(dict.count(key)==0){
+		throw runtime_error("No value in the dictionary!");
+	}
+	Value &v = dict[key];
+	return v;
+}
+int main(int argc, char *argv[])
+{
+	map<int, string> m = {{0, "value"}};
+	string &item = GetRefStrict(m, 0);
+	item = "newvalue";
+	cout << m[0] << endl;
+}
