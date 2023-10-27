63739d594431c756f6cc5bef01c2158f910c5417
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-27 20:19:12 +0300

    Update solution.cpp

--- YellowBelt/Средняя_температура_2/solution.cpp
+++ YellowBelt/Средняя_температура_2/solution.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	vector<int> days;
-	int k, num, sum=0;
+	int k, num, sum=0, mid;
 	cin >> k;
 	for(int i = 0; i < k; i++){
 		cin >> num;
 		sum += num;
 		days.push_back(num);
 	}
+	mid = sum/k;
+	cout << mid << endl;
 	for(int j = 0; j < days.size(); j++){
-		if((sum/k) < days[j]){
-			cout << "День "<<  j << endl;
+		if(mid < days[j]){
+			cout << j << " ";
 		}
 	}
 }
