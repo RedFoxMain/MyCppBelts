20d3a1f1a28406e7c9bbe623501dfdff73813693
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-10-27 19:47:16 +0300

    Update solution.cpp

--- YellowBelt/Масса_блоков/solution.cpp
+++ YellowBelt/Масса_блоков/solution.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	unsigned int N, p, W,H,D, Sum=0;
+	cin >> N >> p;
+	for(int i = 0; i < N; i++){
+		cin >> W >> H >> D;
+		Sum += (W*H*D)*p;
+	}
+	cout << Sum << endl;
+}
