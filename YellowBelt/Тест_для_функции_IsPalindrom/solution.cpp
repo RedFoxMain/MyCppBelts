9f6a122155b6cf2f6b42b1fc1fafbf6ec047d2dd
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-05 15:08:40 +0300

    Update solution.cpp

--- YellowBelt/Тест_для_функции_IsPalindrom/solution.cpp
+++ YellowBelt/Тест_для_функции_IsPalindrom/solution.cpp
@@ -1,1 +1,58 @@
 
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class TestRunner{
+public:
+
+	template<typename A, typename B>
+	void AssertionEqual(A a, B b){
+		ostringstream os;
+		if(a != b){
+			os << a << " != " << b << " Тест: " << test;
+			throw runtime_error(os.str());
+		}
+		test++;
+	}
+	
+private:
+	int test = 1;
+};
+
+// функция для проверки на палиндром
+bool IsPalindrom(const string &str)
+{
+	bool answer = true;
+
+	if (str.size() != 0)
+	{
+		for (int i = 0; i < (str.size() / 2); i++)
+		{
+			int j = str.size() - (i + 1);
+			if (str[i] != str[j])
+			{
+				answer = false;
+				break;
+			}
+		}
+	}
+
+	return answer;
+}
+// тест для функции IsPslindrom
+void TestIsPalindrom(TestRunner &tr, string text, bool result){
+	tr.AssertionEqual(IsPalindrom(text), result);
+}
+
+int main(int argc, char *argv[])
+{
+	TestRunner tr;
+	
+	TestIsPalindrom(tr, "Nikita", false);
+	TestIsPalindrom(tr, "a b a", true);
+	TestIsPalindrom(tr, "c vc", false);
+	TestIsPalindrom(tr, "le v el", true);
+	TestIsPalindrom(tr, "", true);
+	cout << "Ok" << endl;
+}
