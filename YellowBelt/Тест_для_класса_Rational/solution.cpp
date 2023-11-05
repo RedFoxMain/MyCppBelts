3e63b82591b2537894a510edfdd197452df9515b
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-05 14:14:06 +0300

    Update solution.cpp

--- YellowBelt/Тест_для_класса_Rational/solution.cpp
+++ YellowBelt/Тест_для_класса_Rational/solution.cpp
@@ -1,20 +1,17 @@
-
 #include <iostream>
 #include <cassert>
 #include <sstream>
 #include "RationalClass.hpp"
 
 using namespace std;
-class TestRunner
-{
+class TestRunner{
 	int test = 1;
-	public:
+
+  public:
 	template <typename A, typename B>
-	void assertEqual(A a, B b)
-	{
+	void assertEqual(A a, B b){
 		ostringstream os;
-		if (a != b)
-		{
+		if (a != b){
 			os << a << " != " << b << " Тест: " << to_string(test);
 			throw runtime_error(os.str());
 		}
@@ -22,58 +19,39 @@ class TestRunner
 	}
 };
 
-// тест сложения двух дробей
-void TestAdd(TestRunner &tr, Rational &fraction1, Rational &fraction2){
-	tr.assertEqual(Rational::Add(fraction1, fraction2), 0.85); // тестируем сложение дробей
-}
-// тест вычитания двух дробей
-void TestSubdivide(TestRunner &tr, Rational &fraction1, Rational &fraction2){
-	tr.assertEqual(Rational::Subdivide(fraction1, fraction2), -0.35);
-}
-// тест умножения двух дробей
-void TestMultiply(TestRunner &tr, Rational &fraction1, Rational &fraction2){
-	tr.assertEqual(Rational::Multiply(fraction1, fraction2), 0.15);
-}
-// // тест деления двух дробей
-void TestDivide(TestRunner &tr, Rational &fraction1, Rational &fraction2){
-	tr.assertEqual(Rational::Reduce(Rational::Divide(fraction1, fraction2)), 0.42);
-}
 // тест для пустого конструктора
 void TestDefaultFraction(TestRunner &tr){
 	Rational fraction;
 	string test_NullFraction = to_string(fraction.numerator) + "/" + to_string(fraction.denominator);
-	
+
 	tr.assertEqual(test_NullFraction, "0/1");
 }
-
-void TestFraction(TestRunner &tr){
-	Rational fraction(-2,-3);
-	Rational fraction2(7,3);
-	
+// тест знаков у дроби
+void TestFraction(TestRunner &tr, Rational &fraction){
 	string test_Fraction = to_string(fraction.numerator) + "/" + to_string(fraction.denominator);
-	string test_Fraction2 = to_string(fraction2.numerator) + "/" + to_string(fraction2.denominator);
-	
 	tr.assertEqual(test_Fraction, "-2/3");
-	tr.assertEqual(test_Fraction2, "7/3");
 }
-void StartTest()
-{
+// тест сокращения дроби
+void TestReducation(TestRunner &tr, Rational fraction){
+	string testReducationFraction = to_string(fraction.Numerator()) + "/" + to_string(fraction.Denominator());
+	tr.assertEqual(testReducationFraction, "5/1");
+}
+void StartTest(){
 	TestRunner tr;
 	
-	Rational fraction1(3, 5);
-	Rational fraction2(1, 4);
-	
-	// тест математических операций
-	TestAdd(tr, fraction1, fraction2);
-	TestSubdivide(tr, fraction1, fraction2);
-	TestMultiply(tr, fraction1, fraction2);
-	TestDivide(tr, fraction1, fraction2);
+	Rational fraction(-2,3);
+	Rational fraction2(2,-3);
+	Rational fraction3(100, 20);
 	
 	// тест дефолтной дроби
 	TestDefaultFraction(tr);
-	
+
 	// тест знаков у дробей
-	TestFraction(tr);
+	TestFraction(tr, fraction);
+	TestFraction(tr, fraction2);
+	
+	// тест сокращения дроби
+	TestReducation(tr, fraction3);
 	
 	cout << "Ok" << endl;
 }
