60602d7c9d2f0639bb140a592cf1d008723146cd
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-08 10:37:47 +0300

    Update sum_reverse_sort.cpp

--- YellowBelt/Неделя_3/Sort_Reverse_Sum/sum_reverse_sort.cpp
+++ YellowBelt/Неделя_3/Sort_Reverse_Sum/sum_reverse_sort.cpp
@@ -1,1 +1,17 @@
+#include <algorithm>
+#include "sum_reverse_sort.hpp"
 
+using namespace std;
+
+int Sum(int x, int y){
+	return x+y;
+}
+
+string Reverse(string s){
+	reverse(s.begin(), s.end());
+	return s;
+}
+
+void Sort(vector<int>& nums){
+	sort(nums.begin(), nums.end());
+}
