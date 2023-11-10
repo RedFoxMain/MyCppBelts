cd54ebe2d6e62e23c28e7452798d8454314595fe
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-10 19:21:23 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Разбиение_на_слова/solution.cpp
+++ YellowBelt/Неделя_4/Разбиение_на_слова/solution.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
 
+vector<string> SplitIntoWords(const string& s){
+	vector<string> words;
+	string word;
+	stringstream sstream(s);
+	while(!sstream.eof()){
+		getline(sstream, word, ' ');
+		words.push_back(word);
+	}
+	return words;
+}
+
+int main(int argc, char *argv[])
+{
+	string s = "C Cpp Java Python";
+
+    vector<string> words = SplitIntoWords(s);
+    cout << words.size() << " " << endl;
+    for (auto it = begin(words); it != end(words); ++it) {
+      if (it != begin(words)) {
+        cout << "/";
+      }
+      cout << *it;
+    }
+    cout << endl;
+}
