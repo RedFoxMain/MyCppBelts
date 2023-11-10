a4c274478c0774b1b4d14b02cfc43a9a6e07ba5e
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-10 22:48:02 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Часть_множества/solution.cpp
+++ YellowBelt/Неделя_4/Часть_множества/solution.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
 
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border){
+	vector<T> result;
+	for(auto el: elements){
+		if(el > border){
+			result.push_back(el);
+		}
+	}
+	return result;
+}
+
+int main(int argc, char *argv[])
+{
+	for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+      cout << x << " ";
+    }
+    cout << endl;
+  
+    string to_find = "Python";
+    cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+    return 0;
+}
