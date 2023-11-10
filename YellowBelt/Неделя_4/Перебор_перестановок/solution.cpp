2feb061cccfb0c5ea77974131c2742082a1d2721
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-10 14:41:02 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Перебор_перестановок/solution.cpp
+++ YellowBelt/Неделя_4/Перебор_перестановок/solution.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+	int N;
+	vector<int> nums;
+	cin >> N;
+	// наполняем вектор числами от N до 1
+	for(int i = N; i >=1 ; i--){
+		nums.push_back(i);
+	}
+	
+	do{
+		// выводим числа. Чисел в ряду N
+		for(int i = 0; i < N; i++){
+			cout << nums[i] << " ";
+		}
+		cout << endl;
+	}while(prev_permutation(nums.begin(), nums.end()));
+	
+}
