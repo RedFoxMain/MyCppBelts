8bc1723ba4dbc32b0d429a2403637930417ffdc5
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-13 09:15:12 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Построение_арифметического_выражения/Часть_1/solution.cpp
+++ YellowBelt/Неделя_4/Построение_арифметического_выражения/Часть_1/solution.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string expr, operation;
+	int N, num;
+	cin >> expr;
+	cin >> N;
+	for(int i = 0; i < N; i++){
+		cin >> operation;
+		cin.ignore(1);
+		cin >> num;
+		expr = "(" + expr + ") " + operation + " " + to_string(num);
+	}
+	cout << expr << endl;
+}
