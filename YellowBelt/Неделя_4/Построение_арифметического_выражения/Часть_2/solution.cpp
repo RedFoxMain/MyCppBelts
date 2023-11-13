6b6cc3436366c3d8e06ce8b0f6acea72b520bc39
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-13 09:15:28 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Построение_арифметического_выражения/Часть_2/solution.cpp
+++ YellowBelt/Неделя_4/Построение_арифметического_выражения/Часть_2/solution.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <deque>
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+	string expr;
+	char operation, current;
+	int N, num;
+	deque<string> deq;
+	cin >> expr;
+	deq.push_back(expr);
+	
+	cin >> N;
+	for(int i = 0; i < N; i++){
+		cin >> operation;
+		cin.ignore(1);
+		switch(operation){
+			case '+':
+				deq.push_back("+");
+				current = '+';
+				break;
+			case '-':
+				deq.push_back("-");
+				current = '-';
+				break;
+			case '*':
+				if(current == '+' || current == '-'){
+					deq.push_front("(");
+					deq.push_back(")");
+				}
+				deq.push_back("*");
+				current = '*';
+				break;
+			case '/':
+				if(current == '+' || current == '-'){
+					deq.push_front("(");
+					deq.push_back(")");
+				}
+				deq.push_back("/");
+				current = '/';
+				break;
+		}
+		cin >> num;
+		deq.push_back(to_string(num));
+	}
+	for(auto el: deq){
+		cout << el << " ";
+	}
+}
