17726e53ce6605ffabb914f6f679fa8f9aa0b587
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-14 19:44:38 +0300

    Update solution.cpp

--- YellowBelt/Неделя_5/Наследование/solution.cpp
+++ YellowBelt/Неделя_5/Наследование/solution.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+class Animal {
+public:
+    // ваш код
+	Animal(string name): Name(name){}
+    const string Name;
+};
+
+
+class Dog: public Animal {
+public:
+    // ваш код
+	Dog(string name = "Dog"): Animal(name){
+	   
+	} 
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
+int main(){
+	Dog d;
+	d.Bark();
+}
