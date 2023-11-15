17f0ca03be0ae42cefec66186c1e15f88ac212f4
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-15 18:45:57 +0300

    Update solution.cpp

--- WhiteBelt/Сортировка_целых_чисел_по_модулю/solution.cpp
+++ WhiteBelt/Сортировка_целых_чисел_по_модулю/solution.cpp
@@ -4,24 +4,22 @@
 
 using namespace std;
 
-bool Greater(int a, int b){
-	return abs(a) < abs(b); 
-}
-
 int main(int argc, char *argv[])
 {
-	int Q,k;
-	vector<int> nums;
-	
-	cin >> Q;
-	for(int i = 0; i < Q; i++){
-		cin >> k;
-		nums.push_back(k);
-	}
-	
-	sort(nums.begin(), nums.end(), Greater);
-	
-	for(int el: nums){
-		cout << el << " ";
-	}
+        int Q,k;
+        vector<int> nums;
+
+        cin >> Q;
+        for(int i = 0; i < Q; i++){
+                cin >> k;
+                nums.push_back(k);
+        }
+
+        sort(nums.begin(), nums.end(), [](int &a, int &b){
+        	return abs(a) < abs(b);
+        });
+
+        for(int el: nums){
+                cout << el << " ";
+        }
 }
