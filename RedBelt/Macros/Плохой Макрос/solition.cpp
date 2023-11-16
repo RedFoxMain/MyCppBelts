d08954af8974235c4967b923c1407e57852ecfc4
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-11-16 08:25:03 +0300

    Update solition.cpp

--- RedBelt/Macros/Плохой Макрос/solition.cpp
+++ RedBelt/Macros/Плохой Макрос/solition.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
 
+#define PRINT_VALUES(out, x, y) out << (x) << endl; out << (y) << endl;
+
+int main(int argc, char *argv[])
+{
+	ostringstream os;
+	PRINT_VALUES(os, 3, "hello");
+	cout << os.str();
+}
