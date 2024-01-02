15dfcdedcfa4894025a4d5a68962d50b97fa3fa0
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-02 18:23:39 +0300

    Update solution.cpp

--- WhiteBelt/Factorial/solution.cpp
+++ WhiteBelt/Factorial/solution.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
-using namespace std;
-
-int factorial(int N){
-	if(N<=1){
-		return 1;
-	}
-	return N*factorial(N-1);
+long factorial(int N){
+        if(N<=1){ return 1; }
+        return N*factorial(N-1);
 }
 
 int main(int argc, char *argv[])
 {
-	int N;
-	cin >> N;
-	cout << factorial(N);
+        int N;
+        std::cin >> N;
+        std::cout << factorial(N);
 }
