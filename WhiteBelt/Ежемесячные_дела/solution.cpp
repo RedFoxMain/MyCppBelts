d9d14718ee4053f698d16c0d34b77632497c5b44
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-21 20:45:43 +0300

    Update solution.cpp

--- WhiteBelt/Ежемесячные_дела/solution.cpp
+++ WhiteBelt/Ежемесячные_дела/solution.cpp
@@ -1,37 +1,49 @@
 #include <iostream>
 #include <map>
 #include <set>
-using namespace std;
 
 using Notes = std::map<int, std::set<std::string>>;
-using Months = map<int, int>;
+using Months = std::map<int, int>;
 
-int next(int &current){
+// взять максимальный день месяца
+int next(int current){
         Months current_mon = {{1, 31}, {2, 28}, {3, 31}, {4, 30}, {5, 31}, {6, 30}, {7, 31}, {8, 31}, {9, 30}, {10, 31}, {11, 30}, {12, 31}};
         return current_mon[current];
 }
 
+// добавить дело в определенный день
+void ADD(Notes& notes, int current_mon){
+	int day;
+	std::string to_do;
+	std::cin >> day >> to_do;
+	if (day > 0 && day < next(current_mon)) { notes[day].insert(to_do); }
+	else { std::cout << "Неверно введен день: " << day << std::endl; }
+}
+
+// получить дела в определенный день
+void DUMP(Notes& notes, int current_mon){
+	int day;
+	std::cin >> day;
+	if (day < next(current_mon)) { for(const auto& el: notes[day]) { std::cout << el << std::endl; } }
+	else { std::cout << "Неверно введен день: " << day << std::endl; }
+}
+
+// следущий месяц
+void NEXT(Notes& notes, int& current_mon){
+	notes.clear();
+	current_mon++;
+}
+
 int main(){
-	std::string command, to_do;
+	std::string command;
 	Notes notes;
-	int Q, day, current_mon = 1;
+	int Q, current_mon = 1;
 	std::cin >> Q;
 	for(size_t i = 0; i < Q; ++i){
 		std::cin >> command;
-		if(command == "ADD"){
-			std::cin >> day >> to_do;
-			if (day > 0 && day < next(current_mon)) { notes[day].insert(to_do); }
-			else { std::cout << "Неверно введен день: " << day << std::endl; continue; }
-		}
-		if(command == "DUMP"){
-			std::cin >> day;
-			if (day < next(current_mon)) { for(const auto& el: notes[day]) { std::cout << el << std::endl; } }
-			else { std::cout << "Неверно введен день: " << day << std::endl; continue; }
-		}
-		if(command == "NEXT"){
-			notes.clear();
-			current_mon++;
-		}
+		if(command == "ADD"){ ADD(notes, current_mon); continue; }
+		if(command == "DUMP"){ DUMP(notes, current_mon); continue; }
+		if(command == "NEXT"){ NEXT(notes, current_mon); }
 	}
 	return 0;
 }
