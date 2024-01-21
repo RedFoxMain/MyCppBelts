20b9ea255cfe26b0134478e628616698b48848a2
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-21 18:43:26 +0300

    Update solution.cpp

--- WhiteBelt/Второе_вхождение/solution.cpp
+++ WhiteBelt/Второе_вхождение/solution.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-
-using namespace std;
+#include <algorithm>
 
 // второе вхождение f
-int find_symbol(string text, char symb){
-        // если в тексте присутвует f мы возьмеём её позицию и от неё пробуем найти вторую f есои не нпходим возвращаем -1 если нет f вообще -2
-        int target = (text.find(symb) != string::npos) ? text.find(symb, text.find(symb)+1) : -2;
-        return target;
+int find_symbol(const std::string& text, const char& symb){
+	int in_text = std::count(begin(text), end(text), symb);
+	int position_of_first_symbol = text.find(symb);
+	if(in_text == 1) { return -1;}
+	if(in_text == 0) { return -2;}
+	return text.find(symb, position_of_first_symbol + 1);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-        string letter = "informative film";
-        string letter2 = "film";
-        string letter3 = "test";
-        cout << find_symbol(letter, 'f') << endl;
-        cout << find_symbol(letter2, 'f') << endl;
-        cout << find_symbol(letter3, 'f') << endl;
+        std::string letter = "informative film";
+        std::string letter2 = "film";
+        std::string letter3 = "test";
+        std::cout << find_symbol(letter, 'f') << std::endl;
+        std::cout << find_symbol(letter2, 'f') << std::endl;
+        std::cout << find_symbol(letter3, 'f') << std::endl;
 }
