d095c85a6cae3af6ad4532b51ceb281c086deec3
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-03-30 22:02:58 +0300

    Update solution.cpp

--- WhiteBelt/Отбор_палиндромов/solution.cpp
+++ WhiteBelt/Отбор_палиндромов/solution.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <set>
-using namespace std;
 
 // функция возвращает вектор палиндромов, с заданной длиной
-vector<string> PalindromFilter(vector<string> words, int maxLen)
-{
-	vector<string> result;
-	for(const auto el: words){
-		if(el.size() == maxLen){
-			result.push_back(el);
-		}
-	}
-	return result;
+std::vector<std::string> PalindromFilter(const std::vector<std::string> &words, int maxLen){
+        std::vector<std::string> result;
+        for(const auto el: words){
+        	if(el.size() == maxLen){
+            	result.push_back(el);
+            }
+        }
+        return result;
 }
 // проверить палиндром ли это
-bool isPalindrom(string palindrom)
-{
-	string pal = palindrom;
-	reverse(palindrom.begin(), palindrom.end());
-	if(pal==palindrom){
-		return true;
-	}
-	return false;
-
+bool isPalindrom(std::string palindrom){
+	std::string pal = palindrom;
+    std::reverse(palindrom.begin(), palindrom.end());
+    if(pal==palindrom){
+    	return true;
+    }
+    return false;
 }
-int main(int argc, char *argv[])
-{
-	set<string> unique;
-	
-	int Q, len;
-	string palindrom;
-	cout << "Кол-во слов: ";
-	cin >> Q;
-	if (Q < 101)
-	{
-		for (int i = 0; i < Q; i++)
-		{
-			cin >> palindrom;
-			if (isPalindrom(palindrom))
-			{
-				unique.insert(palindrom);
-			}
-		}
-	}
-	else
-	{
-		cout << "Кол-во слов не должно быть болше 100";
-	}
-	cout << "Желаемая длина: ";
-        cin >> len;
-	vector<string> words(unique.begin(), unique.end());
-	for(const auto el: PalindromFilter(words, len)){
-		cout << el << endl;
-	}
+
+int main(){
+        std::set<std::string> unique;
+
+        int Q, len;
+        std::string palindrom;
+        std::cout << "Кол-во слов: ";
+        std::cin >> Q;
+        if (Q < 101){
+        	for (int i = 0; i < Q; i++){
+            	std::cin >> palindrom;
+                if (isPalindrom(palindrom)){
+                	unique.insert(palindrom);
+                }
+            }
+        }
+        else{
+            std::cout << "Кол-во слов не должно быть болше 100";
+        }
+        std::cout << "Желаемая длина: ";
+        std::cin >> len;
+        std::vector<std::string> words(unique.begin(), unique.end());
+        for(const auto el: PalindromFilter(words, len)){
+                std::cout << el << std::endl;
+        }
 }
