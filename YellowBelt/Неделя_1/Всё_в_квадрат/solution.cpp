44411c8bbf6ea5e72569c132ccca7c52778b50d4
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-03-31 22:55:35 +0300

    Update solution.cpp

--- YellowBelt/Неделя_1/Всё_в_квадрат/solution.cpp
+++ YellowBelt/Неделя_1/Всё_в_квадрат/solution.cpp
@@ -1,56 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <map>
-using namespace std;
 
-// перегрузка *
-template<typename First, typename Second> 
-pair<First,Second> operator *(pair<First, Second> &p1, pair<First, Second> &p2){
-	First a = p1.first*p2.first;
-	Second b = p2.second*p2.second;
-	return {a,b};
-}
-// Sqr для pair
-template<typename T> T Sqr(T x){
-	return x*x;
+template<typename T>
+T Sqr(T a){
+	return a*a;
 }
 
-// Sqr для vector
-template<typename T> vector<T> Sqr(vector<T> vec){
-	vector<T> result;
-	for(auto el: vec){
-		result.push_back(el*el);
+template<typename First, typename Second>
+std::pair<First, Second> Sqr(std::pair<First, Second> pair){
+	First first = pair.first*pair.first;
+	Second second = pair.second*pair.second;
+	return {first, second};
+}
+
+template<typename T>
+std::vector<T> Sqr(const std::vector<T> &vec){
+	std::vector<T> result; 
+	for(const auto &el: vec){
+		result.push_back(Sqr(el));
 	}
 	return result;
 }
 
-// Sqr для map
-template<typename T, typename First, typename Second> 
-map<int, pair<int,int>> Sqr(map<T, pair<First, Second>> data){
-	map<T, pair<First, Second>> result;
-	for(auto [key, pair]: data){
-		result[key] = Sqr(pair);
+template<typename T, typename First, typename Second>
+std::map<T, std::pair<First, Second>> Sqr(std::map<T, std::pair<First, Second>> map){
+	std::map<T, std::pair<int, int>> result;
+	for(const auto [key, val]: map){
+		result[key] = Sqr(val);
 	}
 	return result;
 }
 
-int main(int argc, char *argv[])
-{
-	vector<int> v = {1,2,3};
-	cout << "Vector: ";
-	for(auto el: Sqr(v)){
-		cout << el << " ";
+int main(){
+	// Пример вызова функции
+	std::vector<int> v = {1, 2, 3};
+	std::cout << "vector:";
+	for (int x : Sqr(v)) {
+		std::cout << ' ' << x;
 	}
+	std::cout << std::endl;
 	
-	cout << endl;
-	
-	cout << "Map: " <<endl;
-	map<int, pair<int, int>> m = {
-		{4,{2,2}},
-		{7,{4,3}}
+	std::map<int, std::pair<int, int>> map_of_pairs = {
+	  {4, {2, 2}},
+	  {7, {4, 3}}
 	};
-	for(auto [key, pair]: Sqr(m)){
-		cout << key << " " << pair.first << " " << pair.second << endl;
+	std::cout << "map of pairs:" << std::endl;
+	for (const auto& x : Sqr(map_of_pairs)) {
+		std::cout << x.first << ' ' << x.second.first << ' ' << x.second.second << std::endl;
 	}
-	
 }
