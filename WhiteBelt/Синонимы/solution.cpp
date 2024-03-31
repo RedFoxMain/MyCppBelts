db8bd36976851b9db1272654ff51d790f5649328
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-03-31 19:43:16 +0300

    Update solution.cpp

--- WhiteBelt/Синонимы/solution.cpp
+++ WhiteBelt/Синонимы/solution.cpp
@@ -1,77 +1,44 @@
 #include <iostream>
 #include <map>
 #include <set>
-using namespace std;
 
-void commands(string command, map<string, set<string>> &synonimys, set<string> &added)
-{
-	if (command == "ADD")
-	{
-		string word1, word2;
-		cout << "Введите синоним: ";
-		cin >> word1;
-		cout << "Введите синоним: ";
-		cin >> word2;
+using Map = std::map<std::string, std::set<std::string>>;
+Map synonimous;
 
-		synonimys[word1].insert(word2);
-		synonimys[word2].insert(word1);
-
-		added.insert({word1, word2});
-	}
-	else if (command == "COUNT")
-	{
-		string word;
-		cout << "Введите слово: ";
-		cin >> word;
-		cout << synonimys[word].size() << endl;
-	}
-	else if (command == "CHECK")
-	{
-		string word1, word2;
-		cout << "Введите слово: ";
-		cin >> word1;
-		cout << "Введите слово: ";
-		cin >> word2;
-		
-		set<string> words = {word1, word2};
-		bool isSynonimus = false;
-		
-		for (auto [key, values] : synonimys)
-		{
-			if (key == word1 || key == word2)
-			{
-				for (auto value : values)
-				{
-					if (word2 == value || word1 == value)
-					{
-						isSynonimus = true;
-					}
-				}
-			}
-		}
-		if (isSynonimus)
-		{
-			cout << "Синонимы!" << endl;
-		}
-		else
-		{
-			cout << "Не синонимы!" << endl;
-		}
-	}
+void Add(const std::string &word1, const std::string &word2){
+	synonimous[word1].insert(word2);
+	synonimous[word2].insert(word1);
 }
 
-int main(int argc, char *argv[])
-{
+void Count(const std::string &word){
+	std::cout << synonimous[word].size() << std::endl;
+}
+
+void Check(const std::string &word1, const std::string &word2){
+	std::cout << synonimous[word1].count(word2) << std::endl;
+}
+
+int main(){
 	int Q;
-	string command;
-	cin >> Q;
-
-	map<string, set<string>> synonimys;
-	set<string> added;
-
-	for (int i = 0; i < Q; i++)
-	{
-		cin >> command;
-		commands(command, synonimys, added);
+	std::string command;
+	std::cin >> Q;
+	
+	for (int i = 0; i < Q; i++){
+		std::cin >> command;
+		if(command == "ADD"){
+			std::string word1, word2;
+			std::cin >> word1 >> word2;
+			Add(word1, word2);
+		}
+		if(command == "COUNT"){
+			std::string word;
+			std::cin >> word;
+			Count(word);
+		}
+		if(command == "CHECK"){
+			std::string word1, word2;
+			std::cin >> word1 >> word2;
+			Check(word1, word2);
+		}
 	}
 }
