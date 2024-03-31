5557597faceeb641db212d53689c28776694d563
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-03-31 13:15:12 +0300

    Update solution.cpp

--- WhiteBelt/Уравнение/solution.cpp
+++ WhiteBelt/Уравнение/solution.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-	int A,B,C;
-	cin >> A >> B >> C;
-	int D = pow(B, 2) - 4*A*C;
+int main(){
+	int A, B, C;
+	std::cin >> A >> B >> C;
+	int D = std::pow(B, 2) - 4 * A * C;
 	float x1 = 0, x2 = 0;
-	
-	if(D>0 && A!=0){
-		x1 = (-1*B - sqrt(D))/(2*A);
-		x2 = (-1*B + sqrt(D))/(2*A);
+
+	if (D > 0 && A != 0){
+		x1 = (-1 * B - sqrt(D)) / (2 * A);
+		x2 = (-1 * B + sqrt(D)) / (2 * A);
 	}
-	else if(D<0){
-		cout << "Корней нет!" << endl;
+	else if (D < 0){
+		std::cout << "Корней нет!" << std::endl;
 	}
-	else if(D==0 && A!=0){
-		x1 = (-1*B)/(2*A);
+	else if (D == 0 && A != 0){
+		x1 = (-1 * B) / (2 * A);
 	}
-	cout << x1 << " " << x2 << endl;
+	std::cout << x1 << " " << x2 << std::endl;
 }
