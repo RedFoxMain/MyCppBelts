8f69c59776e8e64c72d386d897ff0dba158bdac9
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-04-07 23:04:55 +0300

    Update solution.cpp

--- YellowBelt/Неделя_2/Тест_для_класса_Rational/solution.cpp
+++ YellowBelt/Неделя_2/Тест_для_класса_Rational/solution.cpp
@@ -3,17 +3,16 @@
 #include <sstream>
 #include "RationalClass.hpp"
 
-using namespace std;
 class TestRunner{
 	int test = 1;
 
   public:
 	template <typename A, typename B>
 	void assertEqual(A a, B b){
-		ostringstream os;
+		std::ostringstream os;
 		if (a != b){
-			os << a << " != " << b << " Тест: " << to_string(test);
-			throw runtime_error(os.str());
+			os << a << " != " << b << " Тест: " << std::to_string(test);
+			throw std::runtime_error(os.str());
 		}
 		test++;
 	}
@@ -22,41 +21,40 @@ class TestRunner{
 // тест для пустого конструктора
 void TestDefaultFraction(TestRunner &tr){
 	Rational fraction;
-	string test_NullFraction = to_string(fraction.numerator) + "/" + to_string(fraction.denominator);
+	std::string test_NullFraction = std::to_string(fraction.numerator) + "/" + std::to_string(fraction.denominator);
 
 	tr.assertEqual(test_NullFraction, "0/1");
 }
 // тест знаков у дроби
 void TestFraction(TestRunner &tr, Rational &fraction){
-	string test_Fraction = to_string(fraction.numerator) + "/" + to_string(fraction.denominator);
+	std::string test_Fraction = std::to_string(fraction.numerator) + "/" + std::to_string(fraction.denominator);
 	tr.assertEqual(test_Fraction, "-2/3");
 }
 // тест сокращения дроби
 void TestReducation(TestRunner &tr, Rational fraction){
-	string testReducationFraction = to_string(fraction.Numerator()) + "/" + to_string(fraction.Denominator());
+	std::string testReducationFraction = std::to_string(fraction.Numerator()) + "/" + std::to_string(fraction.Denominator());
 	tr.assertEqual(testReducationFraction, "5/1");
 }
 void StartTest(){
 	TestRunner tr;
-	
-	Rational fraction(-2,3);
-	Rational fraction2(2,-3);
+
+	Rational fraction(-2, 3);
+	Rational fraction2(2, -3);
 	Rational fraction3(100, 20);
-	
+
 	// тест дефолтной дроби
 	TestDefaultFraction(tr);
 
 	// тест знаков у дробей
 	TestFraction(tr, fraction);
 	TestFraction(tr, fraction2);
-	
+
 	// тест сокращения дроби
 	TestReducation(tr, fraction3);
-	
-	cout << "Ok" << endl;
+
+	std::cout << "Ok" << std::endl;
 }
 
-int main(int argc, char *argv[])
-{
+int main(){
 	StartTest();
 }
