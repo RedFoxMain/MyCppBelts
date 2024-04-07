6742572363ba304e91de168fff430e361d783f2b
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-04-07 23:08:12 +0300

    Update solution.cpp

--- YellowBelt/Неделя_2/Тест_для_функции_IsPalindrom/solution.cpp
+++ YellowBelt/Неделя_2/Тест_для_функции_IsPalindrom/solution.cpp
@@ -1,37 +1,31 @@
-
 #include <iostream>
 #include <sstream>
 using namespace std;
 
 class TestRunner{
-public:
-
-	template<typename A, typename B>
+  public:
+	template <typename A, typename B>
 	void AssertionEqual(A a, B b){
 		ostringstream os;
-		if(a != b){
+		if (a != b){
 			os << a << " != " << b << " Тест: " << test;
-			throw runtime_error(os.str());
+			throw std::runtime_error(os.str());
 		}
 		test++;
 	}
-	
-private:
+
+  private:
 	int test = 1;
 };
 
 // функция для проверки на палиндром
-bool IsPalindrom(const string &str)
-{
+bool IsPalindrom(const string &str){
 	bool answer = true;
 
-	if (str.size() != 0)
-	{
-		for (int i = 0; i < (str.size() / 2); i++)
-		{
+	if (str.size() != 0){
+		for (int i = 0; i < (str.size() / 2); i++){
 			int j = str.size() - (i + 1);
-			if (str[i] != str[j])
-			{
+			if (str[i] != str[j]){
 				answer = false;
 				break;
 			}
@@ -41,18 +35,17 @@ bool IsPalindrom(const string &str)
 	return answer;
 }
 // тест для функции IsPslindrom
-void TestIsPalindrom(TestRunner &tr, string text, bool result){
+void TestIsPalindrom(TestRunner &tr, std::string text, bool result){
 	tr.AssertionEqual(IsPalindrom(text), result);
 }
 
-int main(int argc, char *argv[])
-{
+int main(){
 	TestRunner tr;
-	
+
 	TestIsPalindrom(tr, "Nikita", false);
 	TestIsPalindrom(tr, "a b a", true);
 	TestIsPalindrom(tr, "c vc", false);
 	TestIsPalindrom(tr, "le v el", true);
 	TestIsPalindrom(tr, "", true);
-	cout << "Ok" << endl;
+	std::cout << "Ok" << std::endl;
 }
