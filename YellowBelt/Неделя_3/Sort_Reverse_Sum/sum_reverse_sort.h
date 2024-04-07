da26b7a1f4fc8eb228df8f282c56d8b24765d311
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-04-07 23:13:03 +0300

    Update sum_reverse_sort.h

--- YellowBelt/Неделя_3/Sort_Reverse_Sum/sum_reverse_sort.h
+++ YellowBelt/Неделя_3/Sort_Reverse_Sum/sum_reverse_sort.h
@@ -1,10 +1,11 @@
-#pragma once
+#ifndef SUM_REVERSE_SORT_H
+#define SUM_REVERSE_SORT_H
 
 #include <string>
 #include <vector>
 
-using namespace std;
-
 int Sum(int x, int y);
-string Reverse(string s);
-void Sort(vector<int>& nums);
+std::string Reverse(std::string s);
+void Sort(std::vector<int>& nums);
+
+#endif
