3b33e09f0ae87f728f0ab2c3ed329998505a4e59
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-04-10 21:59:19 +0300

    Update solution.cpp

--- YellowBelt/Неделя_4/Группировка_строк_по_префикск/Часть_2/solution.cpp
+++ YellowBelt/Неделя_4/Группировка_строк_по_префикск/Часть_2/solution.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-using namespace std;
 
 template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, const string &prefix)
-{
-	
-	auto start_position = lower_bound(range_begin, range_end, prefix);
-	
+std::pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, const std::string &prefix){
+	auto start_position = std::lower_bound(range_begin, range_end, prefix);
+
 	char second_char = prefix[1];
-    string new_prefix = string(1, prefix[0]) + string(1,  ++second_char);
-    
-	auto end_position = upper_bound(start_position, range_end, new_prefix);
+	std::string new_prefix = std::string(1, prefix[0]) + std::string(1, ++second_char);
 
-	return make_pair(start_position, end_position);
+	auto end_position = std::upper_bound(start_position, range_end, new_prefix);
+
+	return std::make_pair(start_position, end_position);
 }
-int main(int argc, char *argv[])
-{
-	const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
-  
-	const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-	for (auto it = mo_result.first; it != mo_result.second; ++it)
-	{
-		cout << *it << " ";
-	}
-	cout << endl;
-	
-	const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
-  
-  const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-  cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
-  
+int main(){
+	const std::vector<std::string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
 
+	const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+	for (auto it = mo_result.first; it != mo_result.second; ++it){
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+	std::cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << std::endl;
+
+	const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+	std::cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << std::endl;
 }
