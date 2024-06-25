0e0d66331da06fbeeea3caf167caaa39d496ed3b
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-06-25 07:54:42 +0300

    Update solution.cpp

--- YellowBelt/Неделя_5/Набор_фигур/solution.cpp
+++ YellowBelt/Неделя_5/Набор_фигур/solution.cpp
@@ -3,123 +3,112 @@
 #include <sstream>
 #include <cmath>
 #include <iomanip>
-using namespace std;
 
 // Основной класс для фигур
 class Figure{
 public:
-	
-	virtual string Name(){}
-	virtual float Perimeter(){}
-	virtual float Area(){}
+        virtual std::string Name(){}
+        virtual float Perimeter(){}
+        virtual float Area(){}
 };
 
 // клас треугольник
 class Triangle: public Figure{
-	string shape;
-	int a,b,c;
+private:
+        std::string _shape;
+        int _a, _b, _c;
 public:
-	Triangle(string shape, int a, int b, int c){
-		this->shape = shape;
-		this->a = a;
-		this->b = b;
-		this->c = c;
-	}
-	string Name() override {
-		return shape;
-	}
-	float Area() override {
-		float p = (a+b+c)/2;
-		return sqrt(p*(p-a)*(p-b)*(p-c));
-	}
-	float Perimeter() override{
-		return float(a)+float(b)+float(c);
-	}
+        Triangle(std::string shape, int a, int b, int c): _shape(shape), _a(a), _b(b), _c(c) {}
+        std::string Name() override {
+                return _shape;
+        }
+        float Area() override {
+                float perimeter = (_a+_b+_c)/2;
+                return sqrt(perimeter*(perimeter - _a)*(perimeter - _b)*(perimeter - _c));
+        }
+        float Perimeter() override{
+                return float(_a)+float(_b)+float(_c);
+        }
 };
 
 // класс круг
 class Circle: public Figure{
-	string shape;
-	int r;
+private:
+        std::string _shape;
+        int _r;
 public:
-	Circle(string shape, int r){
-		this->shape = shape;
-		this->r = r;
-	}
-	string Name() override {
-		return shape;
-	}
-	float Area() override {
-		return 3.14*pow(r,2);
-	}
-	float Perimeter() override{
-		return 2*3.14*r;
-	}
+        Circle(std::string shape, int r): _shape(shape), _r(r){}
+        std::string Name() override {
+                return _shape;
+        }
+        float Area() override {
+                return 3.14*pow(_r,2);
+        }
+        float Perimeter() override{
+                return 2*3.14*_r;
+        }
 };
 
 // класс прямоугольник
 class Rect: public Figure{
-	string shape;
-	int width, heigth;
+private:
+        std::string _shape;
+        int _width, _heigth;
 public:
-	Rect(string shape, int width, int heigth){
-		this->shape = shape;
-		this->width = width;
-		this->heigth = heigth;
-	}
-	string Name() override {
-		return shape;
-	}
-	float Area() override {
-		return float(width)*float(heigth);
-	}
-	float Perimeter() override{
-		return 2*(float(width) + float(heigth));
-	}
+        Rect(std::string shape, int width, int heigth): _shape(shape), _width(width), _heigth(heigth){}
+        std::string Name() override {
+                return _shape;
+        }
+        float Area() override {
+                return float(_width)*float(_heigth);
+        }
+        float Perimeter() override{
+                return 2*(float(_width) + float(_heigth));
+        }
 };
 
 // функция CreateFigure для получения данных
-shared_ptr<Figure> CreateFigure(istringstream &is){
-	string shape;
-	is >> shape;
-	if(shape == "RECT"){
-		int width, heigth;
-		is >> width >> heigth;
-		shared_ptr<Rect> r(new Rect(shape, width, heigth));
-		return r;
-	}
-	
-	if(shape == "TRIANGLE"){
-		int a,b,c;
-		is >> a >> b >> c;
-		shared_ptr<Triangle> t(new Triangle(shape, a, b, c));
-		return t;
-	}
-	
-	if(shape == "CIRCLE"){
-		int radius;
-		is >> radius;
-		shared_ptr<Circle> c(new Circle(shape, radius));
-		return c;
-		
-	}
+std::shared_ptr<Figure> CreateFigure(std::istringstream &is){
+        std::string shape;
+        is >> shape;
+        if(shape == "RECT"){
+                int width, heigth;
+                is >> width >> heigth;
+                std::shared_ptr<Rect> rect(new Rect(shape, width, heigth));
+                return rect;
+        }
+
+        if(shape == "TRIANGLE"){
+                int a,b,c;
+                is >> a >> b >> c;
+                std::shared_ptr<Triangle> triangle(new Triangle(shape, a, b, c));
+                return triangle;
+        }
+
+        if(shape == "CIRCLE"){
+                int radius;
+                is >> radius;
+                std::shared_ptr<Circle> circle(new Circle(shape, radius));
+                return circle;
+
+        }
 }
 
 int main() {
-  vector<shared_ptr<Figure>> figures;
-  for (string line; getline(cin, line); ) {
-    istringstream is(line);
+  std::vector<std::shared_ptr<Figure>> figures;
+  for (std::string line; std::getline(std::cin, line); ) {
+    std::istringstream is(line);
 
-    string command;
+    std::string command;
     is >> command;
     if (command == "ADD") {
       figures.push_back(CreateFigure(is));
     } else if (command == "PRINT") {
       for (const auto& current_figure : figures) {
-        cout << fixed << setprecision(3)
+        std::cout << std::fixed << std::setprecision(3)
              << current_figure->Name() << " "
              << current_figure->Perimeter() << " "
-             << current_figure->Area() << endl;
+             << current_figure->Area() << std::endl;
       }
     }
   }
