4aa9295480176bd708d0fb56f7a9d6ce340ba85b
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-07-05 22:57:10 +0300

    Update solution_part_2.cpp

--- YellowBelt/Неделя_4/Сортировка_слиянием/solution_part_2.cpp
+++ YellowBelt/Неделя_4/Сортировка_слиянием/solution_part_2.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
+
 
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end){
-	int size_of_vector = range_end-range_begin;
-	if(size_of_vector < 2){
-		return;
-	}else{
-		vector<typename RandomIt::value_type> vec(range_begin, range_end);
-		
-		RandomIt mid1 = begin(vec) + size_of_vector/3;
-		RandomIt mid2 = mid1 + size_of_vector/3;
-		vector<typename RandomIt::value_type> temp;
-			
-		MergeSort(begin(vec), mid1);
-		MergeSort(mid1, mid2);
-		MergeSort(mid2, end(vec));
-		
-		merge(begin(vec), mid1, mid1, mid2, back_inserter(temp));
-		merge(begin(temp), end(temp), mid2, end(vec), range_begin);
-		
-	}
+        int size_of_vector = range_end-range_begin;
+        if(size_of_vector < 2){
+                return;
+        }else{
+                std::vector<typename RandomIt::value_type> vec(range_begin, range_end);
+
+                RandomIt mid1 = std::begin(vec) + size_of_vector/3;
+                RandomIt mid2 = mid1 + size_of_vector/3;
+                std::vector<typename RandomIt::value_type> temp;
+
+                MergeSort(std::begin(vec), mid1);
+                MergeSort(mid1, mid2);
+                MergeSort(mid2, std::end(vec));
+
+                merge(std::begin(vec), mid1, mid1, mid2, std::back_inserter(temp));
+                merge(begin(temp), std::end(temp), mid2, std::end(vec), range_begin);
+
+        }
 }
 
-int main(int argc, char *argv[]){
-	vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 5};
-    MergeSort(begin(v), end(v));
+int main(){
+    std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 5};
+    MergeSort(std::begin(v), std::end(v));
     for (int x : v) {
-      cout << x << " ";
+      std::cout << x << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
     return 0;
 }
