6c60f91c1b415ef5279453fbd800c67f3ef2e26e
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-07-07 22:44:26 +0300

    Update solution.cpp

--- RedBelt/Неделя_1/Максимальный_элемент_по_предикату/solution.cpp
+++ RedBelt/Неделя_1/Максимальный_элемент_по_предикату/solution.cpp
@@ -3,22 +3,19 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
-using namespace std;
-  
+
 template<typename ForwardIterator, typename UnaryPredicate>
-ForwardIterator max_element_if(ForwardIterator first, ForwardIterator l    ast, UnaryPredicate pred){
-    ForwardIterator r = max_element(first, last, pred);
+ForwardIterator max_element_if(ForwardIterator first, ForwardIterator last, UnaryPredicate pred){
+    ForwardIterator r = std::max_element(first, last, pred);
     return r;
 }
 
 int main(){
-        vector<int> array = {4,3,8,12,14};
-        auto a = max_element_if(begin(array), end(array), [](int a, int b){ 16         return a < b && b%2==0;
-        });
-        cout << a-begin(array) << endl;
-   
-        vector<string> text = {"One", "two", "Three", "One", "Two", "Three"    , "one", "Two", "three"};                                               21
-        auto b = max_element_if(begin(text), end(text), [](string a, string     b){                                                                    23         return a.length() < b.length() && isupper(b.front());
-        });
-        cout << b-begin(text) << endl;
+        std::vector<int> array = {4,3,8,12,14};
+        auto a = max_element_if(std::begin(array), std::end(array), [](int a, int b){ return a < b && b%2==0; });
+        std::cout << a-std::begin(array) << std::endl;
+
+        std::vector<std::string> text = {"One", "two", "Three", "One", "Two", "Three", "one", "Two", "three"};
+        auto b = max_element_if(std::begin(text), std::end(text), [](std::string a, std::string b){ return a.length() < b.length() && isupper(b.front());});
+        std::cout << b-std::begin(text) << std::endl;
  }
