69f9e08c03deeb10eb7b9259382085c1ee0c5d7c
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2024-12-23 09:40:18 +0300

    Update solution.cpp

--- WhiteBelt/Rational_class/solution.cpp
+++ WhiteBelt/Rational_class/solution.cpp
@@ -1,162 +1,119 @@
 #include <iostream>
 
-using namespace std;
+class Rational{
+public:
+        int numerator;
+        int denominator;
+        int res;
+        Rational(): numerator(0), denominator(1) {}
+        Rational(int numerator, int denominator){
+                if (denominator == 0){
+                        throw std::invalid_argument("invalid_argument 0");
+                }
+                if (denominator < 0 && numerator > 0){
+                        this->numerator = numerator * -1;
+                        this->denominator = denominator * -1;
+                }
+                else if (denominator < 0 && numerator < 0){
+                        this->numerator = numerator;
+                        this->denominator = denominator * -1;
+                }
+                else{
+                        this->numerator = numerator;
+                        this->denominator = denominator;
+                }
+        }
+        
+public:
+        int Numerator(){
+                return numerator/NOD(numerator, denominator);
+        }
 
-class Rational
-{
-  public:
-	int numerator;
-	int denominator;
-	int res;
-	Rational()
-	{
-		this->numerator = 0;
-		this->denominator = 1;
-	}
-	Rational(int numerator, int denominator)
-	{
-		if (denominator == 0)
-		{
-			throw invalid_argument("invalid_argument 0");
-		}
-		if (denominator < 0 && numerator > 0)
-		{
-			this->numerator = numerator * -1;
-			this->denominator = denominator * -1;
-		}
-		else if (denominator < 0 && numerator < 0)
-		{
-			this->numerator = numerator;
-			this->denominator = denominator * -1;
-		}
-		else
-		{
-			this->numerator = numerator;
-			this->denominator = denominator;
-		}
-	}
-
-	int Numerator()
-	{
-		return numerator/NOD(numerator, denominator);
-	}
-
-	int Denominator()
-	{
-		return denominator/NOD(denominator, numerator);
-	}
-	int NOD(int a, int b)
-	{
-		int c = a;
-		while (a % c != 0 || b % c != 0)
-		{
-			c--;
-		}
-		return c;
-	}
-	// overload operator +
-	friend Rational operator+(Rational &a, Rational &b)
-	{
-		Rational c;
-		c.numerator = (a+b).Numerator();
-		c.denominator = (a+b).Denominator();
-		return c;
-	}
-	// overload operator -
-	friend Rational operator-(Rational &a, Rational &b)
-	{
-		Rational c;
-		c.numerator = (a-b).Numerator();
-		c.denominator = (a-b).Denominator();
-		return c;
-	}
-	// overload operator *
-	friend Rational operator*(Rational &a, Rational &b)
-	{
-		Rational c;
-		c.numerator = (a*b).Numerator();
-		c.denominator = (a*b).Denominator();
-		return c;
-	}
-	// overload operator /
-	friend Rational operator/(Rational &a, Rational &b)
-	{
-		Rational c;
-		c.numerator = (a/b).Numerator();
-		c.denominator = (a/b).Denominator();
-		return c;
-	}
-	// overload operator ==
-	friend bool operator==(Rational &a, Rational &b)
-	{
-		return a.numerator == b.numerator && a.Denominator() == b.Denominator();
-	}
-	// overload operator !=
-	friend bool operator!=(Rational &a, Rational &b)
-	{
-		return a.numerator != b.numerator || a.Denominator() != b.Denominator();
-	}
-	// overload operator <<
-	friend ostream &operator<<(ostream &os, Rational &a)
-	{
-		os << a.Numerator() << "/" << a.Denominator() << endl;
-		return os;
-	}
-	// Sum
-	static double Add(Rational &a, Rational &b)
-	{
-		return double(a.numerator) / double(a.denominator) + double(b.numerator) / double(b.denominator);
-	}
-	// Sub
-	static double Subdivide(Rational &a, Rational &b)
-	{
-		return double(b.numerator) / double(b.denominator) - double(a.numerator) / double(a.denominator);
-	}
-	// Multiply
-	static double Multiply(Rational &a, Rational &b)
-	{
-		return double(a.numerator * b.Numerator()) / double(b.denominator * a.denominator);
-	}
-	// Divide
-	static double Divide(Rational &a, Rational &b)
-	{
-		return double(b.numerator * a.denominator) / double(a.numerator * b.denominator);
-	}
-	// equal
-	static bool Equal(Rational &a, Rational &b)
-	{
-		return a == b;
-	}
-	// not equal
-	static bool NotEqual(Rational &a, Rational &b)
-	{
-		return a != b;
-	}
-	// Reduce fractals
-	static double Reduce(double num)
-	{
-		return round(num * 100) / 100;
-	}
+        int Denominator(){
+                return denominator/NOD(denominator, numerator);
+        }
+        
+        int NOD(int a, int b){
+                int c = a;
+                while (a % c != 0 || b % c != 0){ c--; }
+                return c;
+        }
+        // overload operator +
+        friend Rational operator+(Rational &a, Rational &b){
+                return Rational((a+b).Numerator(), (a+b).Denominator());
+        }
+        // overload operator -
+        friend Rational operator-(Rational &a, Rational &b){
+                return Rational((a-b).Numerator(), (a-b).Denominator());
+        }
+        // overload operator *
+        friend Rational operator*(Rational &a, Rational &b){
+                return Rational((a*b).Numerator(), (a*b).Denominator());
+        }
+        // overload operator /
+        friend Rational operator/(Rational &a, Rational &b){
+                return Rational((a/b).Numerator(), (a/b).Denominator());
+        }
+        // overload operator ==
+        friend bool operator==(Rational &a, Rational &b){
+                return ((a.numerator == b.numerator) && (a.Denominator() == b.Denominator()));
+        }
+        // overload operator !=
+        friend bool operator!=(Rational &a, Rational &b){
+                return ((a.numerator != b.numerator) || (a.Denominator() != b.Denominator()));
+        }
+        // overload operator <<
+        friend std::ostream &operator<<(std::ostream &os, Rational &a){
+                os << a.Numerator() << "/" << a.Denominator() << std::endl;
+                return os;
+        }
+        // Sum
+        static double Add(Rational &a, Rational &b){
+                return double(b.numerator) / double(b.denominator) + double(a.numerator) / double(a.denominator);
+        }
+        // Sub
+        static double Subdivide(Rational &a, Rational &b){
+                return double(b.numerator) / double(b.denominator) - double(a.numerator) / double(a.denominator);
+        }
+        // Multiply
+        static double Multiply(Rational &a, Rational &b){
+                return double(a.numerator * b.Numerator()) / double(b.denominator * a.denominator);
+        }
+        // Divide
+        static double Divide(Rational &a, Rational &b){
+                return double(b.numerator * a.denominator) / double(a.numerator * b.denominator);
+        }
+        // equal
+        static bool Equal(Rational &a, Rational &b){
+                return a == b;
+        }
+        // not equal
+        static bool NotEqual(Rational &a, Rational &b){
+                return a != b;
+        }
+        // Reduce fractals
+        static double Reduce(double num){
+                return round(num * 100) / 100;
+        }
 };
 
-int main(int argc, char *argv[])
-{
+int main(){
         Rational NullFraction;
-        cout << NullFraction << endl;
+        std::cout << NullFraction << std::endl;
 
         Rational fraction(3, 5);
         Rational fraction2(1, 4);
-        //cout << fraction.Numerator() << endl;
-        //cout << fraction.Denominator() << endl;
-        cout << "Сложение: "<< Rational::Add(fraction, fraction2) << endl;
-        cout << "Вычитание: " << Rational::Subdivide(fraction, fraction2) << endl;
-        cout << "Умножение: " << Rational::Reduce(Rational::Multiply(fraction, fraction2)) << endl;
-        cout << "Деление: "<< Rational::Reduce(Rational::Divide(fraction, fraction2)) << endl;
-        cout << "Равны: "<< Rational::Equal(fraction, fraction2) << endl;
-        cout << "Не равны: "<< Rational::NotEqual(fraction, fraction2) << endl;
+        //std::cout << fraction.Numerator() << std::endl;
+        //std::cout << fraction.Denominator() << std::endl;
+        std::cout << "Сложение: "<< Rational::Add(fraction, fraction2) << std::endl;
+        std::cout << "Вычитание: " << Rational::Subdivide(fraction, fraction2) << std::endl;
+        std::cout << "Умножение: " << Rational::Reduce(Rational::Multiply(fraction, fraction2)) << std::endl;
+        std::cout << "Деление: "<< Rational::Reduce(Rational::Divide(fraction, fraction2)) << std::endl;
+        std::cout << "Равны: "<< Rational::Equal(fraction, fraction2) << std::endl;
+        std::cout << "Не равны: "<< Rational::NotEqual(fraction, fraction2) << std::endl;
 
         // overloaded << 
-        cout << fraction;
-        cout << fraction2;
+        std::cout << fraction;
+        std::cout << fraction2;
 
 }
